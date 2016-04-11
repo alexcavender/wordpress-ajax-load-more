762620fdd8ea26b454dc94f4ef616f10f4e8595f
Alex Cavender <alexcavender@users.noreply.github.com>
2016-04-11 11:02:53 -0400

    create functions.php

---
+++ functions.php
@@ -0,0 +1,45 @@
+function load_more_posts() {
+	$nonce_check = check_ajax_referer( 'extra-special', 'security' );
+	
+	if ( $nonce_check ){ 
+
+		$offset = $_POST['offset'];
+	
+		$args = array(
+			'post_type' => 'post',
+			'posts_per_page' => '10',
+			'orderby'	=> 'date',
+			'order'		=> 'DESC',
+			'offset'	=> $offset,
+		);
+
+		$loop = new WP_Query($args);
+
+		if ( $loop->have_posts() ){
+		
+			while( $loop->have_posts() ){
+				$loop->the_post();
+
+				?>
+				<article>
+
+					<a href="<?php echo get_permalink(); ?>">
+						<h2><?php the_title(); ?></h2>
+					</a>
+					
+					<?php the_excerpt(); ?>
+
+				</article>
+				<?php
+
+			}
+			wp_reset_postdata();
+		}
+		wp_die();
+	}
+	else {
+		exit();
+	}
+}
+add_action( 'wp_ajax_load_more_posts', 'load_more_posts' );
+add_action( 'wp_ajax_nopriv_load_more_posts', 'load_more_posts' );
